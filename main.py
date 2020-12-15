3fb9953388fc2bc8c61dddcf1b38d57015fa7456
Florent HUGOUVIEUX <florent.hugouvieux@protonmail.com>
2020-12-15 12:09:03 +0100

    Revert "Save all computed models at once"
    This reverts commit 5b5a521892d8576d5a8f51f8947377e7cddc54f0.

--- main.py
+++ main.py
@@ -35,9 +35,11 @@ def recipient_from_tuple(recipient_tuple):
     return recipient
 
 
-def file_path_to_model(filepath):
+def file_path_to_model(args):
+    filepath, repo = args
     parsed_mail = mailparser.parse_from_file(filepath)
     mail_model = convert_to_model(parsed_mail)
+    repo.save(mail_model)
     return mail_model
 
 
@@ -52,8 +54,7 @@ def main():
 
     processed_count = 0
     for root, dirs, files in os.walk("maildir"):
-        models = pool.map(file_path_to_model, [os.path.join(root, filepath) for filepath in files])
-        repo.save(*models)
+        pool.map(file_path_to_model, [(os.path.join(root, filepath), repo) for filepath in files])
         computed_now = len(files)
         processed_count += computed_now
         print("Processed {0} (+{1}) files. Continuing ...".format(processed_count, computed_now))
